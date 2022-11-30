0ee4f29b63a9f07b17b46f8951b375584155dd80
Igor Bay <bayigor2002@gmail.com>
2022-11-30 15:12:45 +0300

    LR11-12

---
+++ FinalProject/src/test/java/com/bay/ta/page/ProductPage.java
@@ -0,0 +1,40 @@
+package com.bay.ta.page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ProductPage extends AbstractPage
+{
+    private final String BASE_URL = "https://www.nike.com";
+
+    public ProductPage(WebDriver driver)
+    {
+        super(driver);
+        PageFactory.initElements(this.driver, this);
+    }
+
+    @Override
+    public ProductPage openPage()
+    {
+        driver.navigate().to(BASE_URL);
+        driver.findElement(By.xpath("//button[@data-var='acceptBtn1']")).click();
+        driver.findElement(By.xpath("//*[@data-type='click_geoMismatchDismiss']")).click();
+        return this;
+    }
+
+    public ProductPage addToBag(){
+        new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class=\"css-xf3ahq\"]")));
+        driver.findElement(By.xpath("//*[@class=\"css-xf3ahq\"]")).click();
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("window.scrollBy(0,700)", "");
+        new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@aria-label='Add to Bag']")));
+        driver.findElement(By.xpath("//button[@aria-label='Add to Bag']")).click();
+        return this;
+    }
+}
