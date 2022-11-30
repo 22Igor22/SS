c5de6b0eef8578487fc2dbfa7aa55cacb8804719
Igor Bay <bayigor2002@gmail.com>
2022-11-30 17:36:57 +0300

    LR11-12

--- FinalProject/src/test/java/com/bay/ta/tests/CommonConditions.java
+++ FinalProject/src/test/java/com/bay/ta/tests/CommonConditions.java
@@ -1,10 +1,13 @@
 package com.bay.ta.tests;
 
 import com.bay.ta.driver.DriverSingleton;
+import com.bay.ta.utils.TestListener;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+@Listeners({TestListener.class})
 public class CommonConditions {
 
     protected WebDriver driver;
