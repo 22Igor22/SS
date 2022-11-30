c3525fce54b6902a144300b01eae7d4ed6f3d6c3
Igor Bay <bayigor2002@gmail.com>
2022-11-30 17:40:48 +0300

    LR11-12

--- FinalProject/src/test/java/com/bay/ta/page/ProductPage.java
+++ FinalProject/src/test/java/com/bay/ta/page/ProductPage.java
@@ -1,5 +1,7 @@
 package com.bay.ta.page;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -10,6 +12,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class ProductPage extends AbstractPage
 {
     private final String BASE_URL = "https://www.nike.com";
+    private final Logger logger = LogManager.getRootLogger();
 
     public ProductPage(WebDriver driver)
     {
@@ -30,11 +33,13 @@ public class ProductPage extends AbstractPage
         new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
                 .until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class=\"css-xf3ahq\"]")));
         driver.findElement(By.xpath("//*[@class=\"css-xf3ahq\"]")).click();
+        logger.info("Successful sizing");
         JavascriptExecutor jse = (JavascriptExecutor)driver;
         jse.executeScript("window.scrollBy(0,700)", "");
         new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
                 .until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@aria-label='Add to Bag']")));
         driver.findElement(By.xpath("//button[@aria-label='Add to Bag']")).click();
+        logger.info("Element add success");
         return this;
     }
 }
